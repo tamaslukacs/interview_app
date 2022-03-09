a6ad17343514140e70efc8140e24331608333d96
tamaslukacs <lukacstomi91@gmail.com>
2022-03-09 01:43:19 +0100

    substituted write to file failure with invalid file naming, as file locking across platforms is not uniform and portalocker is buggy.

--- tests/unit/test_output_writer.py
+++ tests/unit/test_output_writer.py
@@ -1,5 +1,5 @@
 import os.path
-import portalocker
+# import portalocker
 from interview_app.output_writer import write_to_text_with_newlines
 import unittest
 from interview_app import config
@@ -7,14 +7,15 @@ import os
 import pytest
 from tutils import get_file_contents_as_list
 
+
 class TestOutputWriter(unittest.TestCase):
 
     def setUp(self):
-        #store temporarily created result file in this variable
+        # store temporarily created result file in this variable
         self.filepath_created = None
 
     def tearDown(self):
-        #cleanup result file after test has run
+        # cleanup result file after test has run
         if self.filepath_created is not None:
             try:
                 os.remove(self.filepath_created)
@@ -26,24 +27,16 @@ class TestOutputWriter(unittest.TestCase):
         resultfile = "res.txt"
 
         self.filepath_created = resultfile
-        write_to_text_with_newlines(answer_list,resultfile )
+        write_to_text_with_newlines(answer_list, resultfile)
 
         assert os.path.isfile(resultfile)
         assert get_file_contents_as_list(resultfile) == answer_list
 
     def test_write_output_invalid_file(self):
         answer_list = ["a", "b", "c"]
-        resultfile = "res.txt"
+        resultfile = r"/*- \res.txt"
         self.filepath_created = resultfile
-        if not os.path.isfile(resultfile):
-            try:
-                with open(resultfile, "w")as f:
-                    f.write("")
-            except Exception as e:
-                print(e)
         with pytest.raises(RuntimeError) as pytest_wrapped_e:
-
-            with portalocker.Lock(resultfile, 'w', portalocker.LOCK_EX) as fh:
-                write_to_text_with_newlines(answer_list,resultfile)
-
-
+            # with portalocker.Lock(filename=resultfile, mode="w", flags=portalocker.LOCK_EX) as fh:
+            # portalocker fails on ubuntu for some reason
+            write_to_text_with_newlines(answer_list, resultfile)
